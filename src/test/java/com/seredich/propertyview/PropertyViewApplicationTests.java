8868c11ffe7e856ad1c9be563bd8f53c154a1d2d
Ilya Seredich <seredichilya2010@gmail.com>
2026-02-14 19:30:15 +0300

    initial commit

---
+++ src/test/java/com/seredich/propertyview/PropertyViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seredich.propertyview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
